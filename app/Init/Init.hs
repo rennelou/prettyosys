5e3d231eafbd451fdb6fea0ba9f39a43f65b9bc8
rlou <rlou@americastg.com>
2022-07-09 11:54:04 -0300

    examples on init

--- app/Init/Init.hs
+++ app/Init/Init.hs
@@ -29,3 +29,57 @@ convertToSettings initArgs =
     (T.pack $ srcDirArg initArgs)
     (T.pack $ vunitsDirArgs initArgs)
     (dephtArg initArgs)
+
+counterRTLFilename = "counter.vhd"
+
+counterRTL =
+  "library IEEE;\n\
+  \use IEEE.std_logic_1164.all;\n\
+  \use IEEE.numeric_std.all;\n \
+  \\n \
+  \entity counter is\n \
+  \  port(\n \
+  \      clk   : in std_logic;\n \
+  \      rst   : in std_logic;\n \
+  \      value : out std_logic_vector(7 downto 0)\n \
+  \  );\n \
+  \end entity counter;\n \
+  \\n \
+  \architecture counter_rtl of counter  is\n \
+  \\n \
+  \  signal counter_reg, counter_next : unsigned(7 downto 0) := (others => '0');\n \
+  \\n \
+  \begin\n \
+  \\n \
+  \  TICK: process (clk)\n \
+  \  begin\n \
+  \      if rising_edge(clk) then\n \
+  \          if rst = '1' then\n \
+  \              counter_reg <= (others => '0');\n \
+  \          else\n \
+  \              counter_reg <= counter_next;\n \
+  \          end if;\n \
+  \      end if;\n \
+  \  end process TICK;\n \
+  \\n \
+  \  counter_next <= counter_reg + 1\n \
+  \                  when counter_reg < 15\n \
+  \                  else (others => '0');\n \
+  \\n \
+  \  value <= std_logic_vector(counter_reg);\n \
+  \\n \
+  \end architecture counter_rtl;"
+
+counterPropertiesFilename = "counter.psl"
+
+counterProperties =
+  "vunit counter_vu(counter(counter_rtl))\n\
+  \{\n\
+  \  default clock is rising_edge(clk);\n\
+  \\n\
+  \  assume rst = '1';\n\
+  \\n\
+  \  COUNT_TO_FIVE : cover {\n\
+  \    value = std_logic_vector(to_unsigned(5, 8))\n\
+  \  };\n\
+  \}"
